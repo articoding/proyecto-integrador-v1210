827b6b770a409af09ce842e4a8ff14ceb935928a
articoding <144624806+articoding@users.noreply.github.com>
2023-10-25 20:47:15 -0500

    cambios de luis emergidos

--- login.php
+++ login.php
@@ -1,13 +1,19 @@
 <?php
+include("connect.php");
     include("connection.php");
     if(isset($_POST['submit'])){
-        $user_name = $_POST['user'];
+        $user_email = $_POST['email'];
         $password = $_POST['password'];
 
-        $sql = "SELECT * FROM users WHERE user_name = '$user_name' AND user_password = '$password'";
+        $sql = "SELECT * FROM users WHERE user_email = '$user_email' AND user_password = '$password'";
         $result = mysqli_query($conn, $sql);
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
         $count = mysqli_num_rows($result);
+        
+            if (password_verify($password, $user_password)) {
+                die("Login successful");
+            }
+        
         if($count == 1){
             header("location: welcome.html");
         }
@@ -19,5 +25,6 @@
     
         }
     }
+    
     
 ?>
