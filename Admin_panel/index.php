5c2f617bd69b0c01695475f2ec06305aa64e90c3
Luis098rivera <luis.rivera040606@gmail.com>
2023-10-26 19:26:03 -0500

    Login de user y admin, preparacion del panel de admin

---
+++ Admin_panel/index.php
@@ -0,0 +1,10 @@
+<?php require_once "View/Parte_superior.php"?>
+
+<!--INICIO del contenido principal-->
+<div class="container">
+    <h1>Contenido Principal</h1>
+</div>
+<!--FIN del contenido principal-->
+
+<?php require_once "View/Parte_inferior.php"?>
+           
