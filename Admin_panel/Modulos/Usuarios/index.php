fa6bd7ddb6ef672f2fd64618721fad3986d4a58a
articoding <144624806+articoding@users.noreply.github.com>
2023-11-09 11:27:03 -0600

    sign in fixed

---
+++ Admin_panel/Modulos/Usuarios/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+?>
+
+
+<?php include("../../Templates/header4.php"); ?>
+
+
+<?php include("../../Templates/footer3.php"); ?>
--- login.php
+++ login.php
@@ -1,30 +1,31 @@
 <?php
-include("connect.php");
-    include("connection.php");
-    if(isset($_POST['submit'])){
-        $user_email = $_POST['email'];
-        $password = $_POST['password'];
+        include("connection.php");
+        if(isset($_POST['submit'])){
+            $user_email = $_POST['email'];
+            $password = $_POST['password'];
 
-        $sql = "SELECT * FROM users WHERE user_email = '$user_email' AND user_password = '$password'";
-        $result = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-        $count = mysqli_num_rows($result);
+            $sql = "SELECT * FROM users WHERE user_email = '$user_email' AND user_password = '$password'";
+            $result = mysqli_query($conn, $sql);
+            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            $count = mysqli_num_rows($result);
+            
+                if ($row["usertype"]=="user") {
+                    header("location: index.php");
+                }
+                
+                elseif($row["usertype"]=="admin")
+                {
+                    header("location: Admin_panel/index.php");
+                }
+            else{
+                echo '<script>
+                    window.location.href = "signin.php";
+                    alert("Inicio de sesión fallido. Correo y/o contraseña inválidos. Intente de nuevo.");
+                </script>';
         
-            if (password_verify($password, $user_password)) {
-                die("Login successful");
             }
+        }
         
-        if($count == 1){
-            header("location: index.php");
-        }
-        else{
-            echo '<script>
-                window.location.href = "signin.php";
-                alert("Inicio de sesión fallido. Contraseña o usuario incorrecto");
-            </script>';
     
-        }
-    }
-    
-    
-?>
+        
+    ?>
