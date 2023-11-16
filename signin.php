08e89d2757d584d4bf56dcb4778872605b174c45
articoding <144624806+articoding@users.noreply.github.com>
2023-11-16 14:19:44 -0600

    Update signin.php

--- signin.php
+++ signin.php
@@ -11,15 +11,15 @@
     <link rel="stylesheet" href="css/styleIS.css">
     <link rel="icon" href="images/fevicon.jpg" type="image/jpg" />
 
-</head>
+  </head>
 <body>
   <div class="myformulario">
     <h1>Inicio de Sesión</h1>
     <form name = "form" action="login.php" method="POST">
       <div class="username">
         <!--<label >Correo</label>-->
-        <input type="text" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}
-          " required id="email" name="email" placeholder="Email">
+        <input type="text" 
+           required id="email" name="email" placeholder="Email">
       </div>
       <div class="username">
         <!--<label>Contraseña</label>-->
